7d3ccda95bfcb1aa957f895b5f902cca517dbb25
Shahar Talmi <shahar.talmi@gmail.com>
2018-04-28 23:53:34 +0300

    add missing

--- dist/bin/npm-ci-build.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var _build = require('../src/build');
-
-(0, _build.build)();
--- dist/test/index.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _chai = require('chai');
-
-describe('nothing', function () {
-  it('should work', function () {
-    (0, _chai.expect)(0).to.equal(0);
-  });
-});
