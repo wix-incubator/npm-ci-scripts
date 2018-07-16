bec73ee14b52821a4fd68e970ff2b9557991665d
johnny <yonatanme@wix.com>
2018-07-16 15:28:35 +0300

    allow to run NPM in "debug mode" using .env file

--- src/build.js
+++ src/build.js
@@ -6,6 +6,13 @@ function npmVersion() {
   return parseFloat(execSync('npm --version | cut -d. -f1,2').toString());
 }
 
+function getNpmConfigFlags(shouldRunInDebug) {
+  return [
+    '--cache ~/.npm.$(npm --version)',
+    ...(shouldRunInDebug ? ['--prefer-online', '--loglevel verbose'] : [])
+  ].join(' ');
+}
+
 export function build() {
   if (process.env.APPLITOOLS_GITHUB_FT) {
     setApplitoolsId();
@@ -15,15 +22,21 @@ export function build() {
     execCommand('yarn install --frozen-lockfile', 'yarn install', 2);
   } else if (fileExists('.yarnrc')) {
     execCommand('yarn install', 'yarn install', 2);
-  } else if (fileExists('package-lock.json')) {
-    if (npmVersion() >= 5.7) {
-      execCommand('npm ci --cache ~/.npm.$(npm --version)', 'npm ci', 2);
-    } else {
-      execCommand('npm install --cache ~/.npm.$(npm --version)', 'npm install', 2);
-    }
   } else {
-    execCommand('npm install --no-package-lock --cache ~/.npm.$(npm --version)', 'npm install', 2);
+    const shouldRunInDebug = !!process.env.NPM_CI_DEBUG;
+    console.log(`NPM running in debug mode: ${shouldRunInDebug}`);
+    const npmConfigFlags = getNpmConfigFlags(shouldRunInDebug);
+    if (fileExists('package-lock.json')) {
+      if (npmVersion() >= 5.7) {
+        execCommand(`npm ci ${npmConfigFlags}`, 'npm ci', 2);
+      } else {
+        execCommand(`npm install ${npmConfigFlags}`, 'npm install', 2);
+      }
+    } else {
+      execCommand(`npm install --no-package-lock ${npmConfigFlags}`, 'npm install', 2);
+    }
   }
+
   execCommand('npm run build --if-present');
 
   if (process.env.agentType === 'pullrequest') {
