58cbd1eb2e72506b3f195c012de4fb9aff3bc13a
johnny <yonatanme@wix.com>
2018-07-25 22:44:49 +0300

    allow to run Yarn in "debug mode" using .env file as well

--- src/build.js
+++ src/build.js
@@ -14,17 +14,24 @@ function getNpmConfigFlags(shouldRunInDebug) {
 }
 
 export function build() {
+  const shouldRunInDebug = process.env.NPM_CI_DEBUG === 'true';
+  console.log(`build in debug mode: ${shouldRunInDebug}`);
+
   if (process.env.APPLITOOLS_GITHUB_FT) {
     setApplitoolsId();
   }
 
-  if (fileExists('yarn.lock')) {
-    execCommand('yarn install --frozen-lockfile', 'yarn install', 2);
-  } else if (fileExists('.yarnrc')) {
-    execCommand('yarn install', 'yarn install', 2);
+  if (fileExists('yarn.lock') || fileExists('.yarnrc')) {
+    if (shouldRunInDebug) {
+      execCommand('yarn cache clean');
+    }
+    const yarnConfigFlag = shouldRunInDebug ? '--verbose' : '';
+    if (fileExists('yarn.lock')) {
+      execCommand(`yarn install --frozen-lockfile ${yarnConfigFlag}`, 'yarn install', 2);
+    } else {
+      execCommand(`yarn install ${yarnConfigFlag}`, 'yarn install', 2);
+    }
   } else {
-    const shouldRunInDebug = !!process.env.NPM_CI_DEBUG;
-    console.log(`NPM running in debug mode: ${shouldRunInDebug}`);
     const npmConfigFlags = getNpmConfigFlags(shouldRunInDebug);
     if (fileExists('package-lock.json')) {
       if (npmVersion() >= 5.7) {
