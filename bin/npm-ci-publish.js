04112ff4999c4a2592ce961df5304c472b1aeaaa
Shahar Talmi <shahart@wix.com>
2018-11-14 05:12:34 +0200

    better status

--- bin/npm-ci-publish.js
+++ bin/npm-ci-publish.js
@@ -7,6 +7,7 @@ execCommand('npm run release --if-present');
 const pkg = readJsonFile('package.json');
 if (pkg.private) {
   console.log('Skipping publish (probably no change in tarball)');
+  console.log(`##teamcity[buildStatus status='SUCCESS' text='{build.status.text}; No publish']`);
 } else {
   logBlockOpen('npm publish');
   publish();
