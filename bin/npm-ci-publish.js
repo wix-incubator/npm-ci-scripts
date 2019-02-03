68d4fa0a0da72c2b39cc5acdf8df0a5103a2e70c
Yair Haimovitch <yairh@wix.com>
2019-02-03 17:50:53 +0200

    run yarn release if using yarn

--- bin/npm-ci-publish.js
+++ bin/npm-ci-publish.js
@@ -1,10 +1,16 @@
+import {get} from 'lodash';
 import {publish} from '../src/publish';
 import {publishScoped} from '../src/publish-scoped';
-import {logBlockOpen, logBlockClose, execCommand, readJsonFile} from '../src/utils';
-
-execCommand('npm run release --if-present');
+import {logBlockOpen, logBlockClose, execCommand, readJsonFile, fileExists} from '../src/utils';
 
 const pkg = readJsonFile('package.json');
+
+if (fileExists('yarn.lock') && get(pkg, 'scripts.release')) {
+  execCommand('yarn release');
+} else {
+  execCommand('npm run release --if-present');
+}
+
 if (pkg.private) {
   console.log('Skipping publish (probably no change in tarball)');
   console.log(`##teamcity[buildStatus status='SUCCESS' text='{build.status.text}; No publish']`);
