017ec635db998e5221e465d5971c2affaa38615d
Yury Michurin <yurym@wix.com>
2019-02-21 15:58:43 +0200

    Fix lint errors

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
-import {writeFileSync, readFileSync, statSync} from 'fs';
+import {writeFileSync, readFileSync, statSync, createReadStream} from 'fs';
 import {execSync, exec} from 'child_process';
 import request from 'request';
 import hostname from 'os';
+import zlib from 'zlib';
 
 export function logBlockOpen(log) {
   console.log('##teamcity[blockOpened name=\'' + log + '\']');
@@ -53,94 +54,100 @@ export function readJsonFile(name) {
 
 function readCompressedToBuffer(path) {
   return new Promise((resolve, reject) => {
-      const gzipStream = fs.createReadStream(path).pipe(zlib.createGzip({
-          level: zlib.constants.Z_BEST_COMPRESSION
-      }));
-      const buffers = [];
+    const gzipStream = createReadStream(path).pipe(zlib.createGzip({
+      level: zlib.constants.Z_BEST_COMPRESSION
+    }));
+    const buffers = [];
 
-      gzipStream.on('data', (data) => { buffers.push(data); })
-      gzipStream.on('error', (err) => { reject(err); })
-      gzipStream.on('end', () => { resolve(Buffer.concat(buffers)) })
+    gzipStream.on('data', data => {
+      buffers.push(data);
+    });
+    gzipStream.on('error', err => {
+      reject(err);
+    });
+    gzipStream.on('end', () => {
+      resolve(Buffer.concat(buffers));
+    });
   });
 }
 
 function sendFileToSlack(path, title, filename) {
-  console.log(`Sending ${path} as "${title}" - ${filename}.gz`)
+  console.log(`Sending ${path} as "${title}" - ${filename}.gz`);
 
   return readCompressedToBuffer(path).then(compressedFile => {
-      return new Promise((resolve, reject) => {
-          request.post({
-              url: 'https://slack.com/api/files.upload',
-              formData: {
-                  token: 'NEED_A_TOKEN_HERE',
-                  title,
-                  channels: 'CFM2ER0DU',
-                  file: {
-                      value: compressedFile,
-                      options: {
-                          filename: filename + '.gz',
-                          contentType: 'application/octet-stream',
-                      }
-                  }
-              },
-          }, function (err, response) {
-              if (err) {
-                return reject(err);
-              }
+    return new Promise((resolve, reject) => {
+      request.post({
+        url: 'https://slack.com/api/files.upload',
+        formData: {
+          token: 'NEED_A_TOKEN_HERE',
+          title,
+          channels: 'CFM2ER0DU',
+          file: {
+            value: compressedFile,
+            options: {
+              filename: filename + '.gz',
+              contentType: 'application/octet-stream',
+            }
+          }
+        },
+      }, (err, response) => {
+        if (err) {
+          return reject(err);
+        }
 
-              try {
-                const body = JSON.parse(response.body);
-                resolve(body);
-              } catch(ex) {
-                reject(ex);
-              }
-          });
-        });
+        try {
+          const body = JSON.parse(response.body);
+          resolve(body);
+        } catch (ex) {
+          reject(ex);
+        }
+      });
+    });
   });
 }
 
-const npmLogPathRegex = /(\/.+\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}_\d{1,3}Z-debug\.log)/g
+const npmLogPathRegex = /(\/.+\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}_\d{1,3}Z-debug\.log)/g;
 export function execCommandAsync(cmd, log, retries, retryCmd) {
-    return new Promise((resolve, reject) => {
-      log = log || cmd;
-      logBlockOpen(log);
-      const childProcess = exec(cmd, {stdio: 'pipe'},  async (error, _, stderr) => {
-        logBlockClose(log);
-        if (error) {
-          const match = npmLogPathRegex.exec(stderr.toString());
-          if (match) {
-            const logPath = match[0];
-            console.log('NPM log path detected:', logPath);
-            const pathParts = logPath.split('/');
-            const filename = pathParts[pathParts.length -1];
-            try {
-              await sendFileToSlack(logPath, `NPM log from ${hostname()} building ${process.env.ARTIFACT_ID} (${process.env.BUILD_VCS_NUMBER})`, filename);
-              console.log('npm log sent to slack!');
-            } catch (error) {
-              console.log('Sending npm log to slack failed', error);
-            }
+  return new Promise((resolve, reject) => {
+    log = log || cmd;
+    logBlockOpen(log);
+    const childProcess = exec(cmd, {stdio: 'pipe'}, async (error, _, stderr) => {
+      logBlockClose(log);
+      if (error) {
+        const match = npmLogPathRegex.exec(stderr.toString());
+        if (match) {
+          const logPath = match[0];
+          console.log('NPM log path detected:', logPath);
+          const pathParts = logPath.split('/');
+          const filename = pathParts[pathParts.length - 1];
+          try {
+            await sendFileToSlack(logPath, `NPM log from ${hostname()} building ${process.env.ARTIFACT_ID} (${process.env.BUILD_VCS_NUMBER})`, filename);
+            console.log('npm log sent to slack!');
+          } catch (error) {
+            console.log('Sending npm log to slack failed', error);
           }
+        }
 
-          if (retries > 0) {
-            if (retryCmd) {
-              console.log('running:', retryCmd);
-              execSync(retryCmd, {stdio: 'inherit'});
-            }
-            const retry = parseInt((log.match(/ \(retry (\d+)\)/) || ['', '0'])[1], 10);
-            log = log.replace(/ \(retry (\d+)\)/, '') + ' (retry ' + (retry + 1) + ')';
-            return asyncExec(cmd, log, retries - 1);
+        if (retries > 0) {
+          if (retryCmd) {
+            console.log('running:', retryCmd);
+            execSync(retryCmd, {stdio: 'inherit'});
           }
+          const retry = parseInt((log.match(/ \(retry (\d+)\)/) || ['', '0'])[1], 10);
+          log = log.replace(/ \(retry (\d+)\)/, '') + ' (retry ' + (retry + 1) + ')';
+          return execCommandAsync(cmd, log, retries - 1);
+        }
 
-          process.exit(error.code);
-          return reject(error);
-        };
+        process.exit(error.code);
+        return reject(error);
+      }
 
-        resolve();
-      });
-
-      console.log('running:', cmd);
-
-      childProcess.stderr.pipe(process.stderr);
-      childProcess.stdout.pipe(process.stdout);
+      resolve();
     });
+
+    console.log('running:', cmd);
+
+    childProcess.stderr.pipe(process.stderr);
+    childProcess.stdout.pipe(process.stdout);
+  });
 }
