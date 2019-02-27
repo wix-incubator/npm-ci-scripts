2b527078e5be1ebff3f0e379c64734cef958c5f9
Shahar Talmi <shahart@wix.com>
2019-02-27 07:58:54 +0200

    pach to allow syncing versions manually

--- bin/npm-ci-publish.js
+++ bin/npm-ci-publish.js
@@ -1,14 +1,34 @@
 import {publish} from '../src/publish';
 import {publishScoped} from '../src/publish-scoped';
 import {logBlockOpen, logBlockClose, execCommand, readJsonFile, writeJsonFile} from '../src/utils';
+import {writeFileSync, unlinkSync} from 'fs';
+import {execSync} from 'child_process';
+
+function latest(registry) {
+  try {
+    const result = JSON.parse(execSync(`npm show --json --registry=${registry} --@wix:registry=${registry}`, {stdio: 'pipe'}).toString());
+    return result['dist-tags'].latest;
+  } catch (error) {
+    return null;
+  }
+}
 
 let pkg = readJsonFile('package.json');
 const previousVersion = pkg.version;
+let shouldUnlink = false;
 if (pkg.name.indexOf('@wix/') === 0) {
   pkg.publishConfig = {registry: 'https://registry.npmjs.org/'};
   writeJsonFile('package.json', pkg);
+  if (latest('http://npm.dev.wixpress.com/') !== latest('https://registry.npmjs.org/')) {
+    console.log('forcing npmjs to trust latest from npmjs');
+    writeFileSync('.npmrc', '@wix:registry=https://registry.npmjs.org/'); //trust latest from npmjs
+    shouldUnlink = true;
+  }
 }
 execCommand('npm run release --if-present');
+if (shouldUnlink) {
+  unlinkSync('.npmrc');
+}
 pkg = readJsonFile('package.json');
 if (pkg.private && previousVersion !== pkg.version) {
   console.log('forcing republish in order to sync versions');
